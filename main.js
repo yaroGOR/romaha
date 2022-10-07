48c1797e4e0b97dfd396908bf86e1889f2447fc0
Yarik <36842297+yaroGOR@users.noreply.github.com>
2022-10-07 14:19:14 +0300

    first com2

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import {GLTFLoader} from 'THREE/examples/jsm/loaders/GLTFLoader.js'
+
 
 // Setup
 
@@ -18,6 +20,9 @@ camera.position.setZ(30);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
+//load model
+const loader = new GLTFLoader();
+
 
 // Torus
 
@@ -29,12 +34,24 @@ scene.add(torus);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
+
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+scene.add( ambientLight);
 
+
+loader.load( './romaha3d.glb', function ( drom ) {
+const romaha=drom.scene.children[0]
+romaha.scale.set(1,1,1);
+
+	scene.add( romaha );
+  romaha.position.set(-10,3,20)
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 // Helpers
 
 // const lightHelper = new THREE.PointLightHelper(pointLight)
@@ -86,7 +103,7 @@ const moon = new THREE.Mesh(
 
 scene.add(moon);
 
-moon.position.z = 30;
+moon.position.z = 20;
 moon.position.setX(-10);
 
 jeff.position.z = -5;
